f441fbc0c1b5a1e3ba7b66dfec77a0a508846b58
pengchao2022 <pengchao.ma2@outlook.com>
2026-06-07 12:56:19 +0800

    feat: GTP-54273 modified the code

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ module "s3-bucket" {
   source = "git::https://github.com/pengchao2022/aws-terraform-modules.git//modules/s3?ref=s3-1.0"
   bucket_name = "maxwell-s3-bucket-20260607" # This is a globally unique identifier for AWS S3 services
   tags = {
-    Name = "R&D bucket" # This is for human reading show in aws console
+    Name = "R-D bucket" # This is for human reading show in aws console
     Demo = "For calling module demo"
     
   }
