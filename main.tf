ba60b04258da8f5024db777d6b82c9c837f60511
pengchao2022 <pengchao.ma2@outlook.com>
2026-06-07 16:01:39 +0800

    feat: GTP-54273 modified the code

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 # call a module
 module "s3-bucket" {
-  source = "git::https://github.com/pengchao2022/aws-terraform-modules.git//modules/s3?ref=s3-1.2"
+  source = "git::https://github.com/pengchao2022/aws-terraform-modules.git//modules/s3?ref=s3-1.3"
   bucket_name = "maxwell-s3-bucket-20260607" # This is a globally unique identifier for AWS S3 services
   # enable s3 static website
   enable_website = true
