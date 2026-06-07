f6191abd2ce15bca512c3e806b8b23e94528d5bd
pengchao2022 <pengchao.ma2@outlook.com>
2026-06-07 15:42:27 +0800

    feat: GTP-54273 modified the code

--- main.tf
+++ main.tf
@@ -5,8 +5,10 @@ provider "aws" {
 
 # call a module
 module "s3-bucket" {
-  source = "git::https://github.com/pengchao2022/aws-terraform-modules.git//modules/s3?ref=s3-1.0"
+  source = "git::https://github.com/pengchao2022/aws-terraform-modules.git//modules/s3?ref=s3-1.1"
   bucket_name = "maxwell-s3-bucket-20260607" # This is a globally unique identifier for AWS S3 services
+  # enable s3 static website
+  enable_website = true
   tags = {
     Name = "R-D-bucket" # This is for human reading show in aws console do not use space
     Demo = "For calling module demo"
